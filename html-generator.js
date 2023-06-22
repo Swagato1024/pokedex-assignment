dd13ce9023cd534303ba2c012dfd3e99a64f67d9
Swagato Chakraborty <Swagato1024@users.noreply.github.com>
2023-06-22 22:14:38 +0530

    Replicated transition to all cards

--- html-generator.js
+++ html-generator.js
@@ -1,89 +1,77 @@
 const fs = require("fs");
-let imgIndex = 1;
-const closingTd = "</td>";
-const opeingTr = "<tr>";
-const closingTr = "</tr>";
-const openingTable = '<table class="card-elements">';
-const closingTable = "</table>";
-const img = '<div class="poke-img card-elements"></div>';
-const openingP = '<p class="name card-elements">';
-const closingP = "</p>";
-const opeingCard = '<div class="card">';
-const closingCard = "</div>";
-const opeingCardRow = '<div class="card-rows">';
-const closingDiv = "</div>";
 
-const generateCard = (line) => {
-  const [id, ...stats] = line.split("|");
+const imgId = (id) => id.toString().padStart(3, 0);
 
-  const pokemon = {
-    Name: stats[0],
-    Types: stats[1],
-    HP: stats[3],
-    XP: stats[4],
-    Attack: stats[5],
-    Defense: stats[6],
-    Weight: stats[7],
-  };
+const generateCard = (stats) => {
+  const [id, name, types, speed, hp, xp, attack, defense, weight] = stats;
 
-  const label = ["Types", "Weight", "HP", "XP", "Attack", "Defense"];
-  const openingTr = "<tr>";
-  const openingTdAttribute = '<td class="label">';
-  const openingTdvalue = '<td class="value">';
+  const typesTemplate = types
+    .split(",")
+    .map((type) => `<div class="${type} poke-types"> ${type} </div>`)
+    .join("\n");
 
-  const rows = label
-    .map((attribute) => {
-      let cardRow = openingTr;
-      cardRow += openingTdAttribute + attribute + closingTd;
-      cardRow += openingTdvalue + pokemon[attribute] + closingTd;
-      cardRow += closingTr;
-      return cardRow;
-    })
-    .join("");
+  return `<div class="card">
+<div class="poke-container">
+  <img
+    src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${imgId(
+      id
+    )}.png"
+    class="poke-img"
+  />
+</div>
 
-  const imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${(imgIndex++)
-    .toString()
-    .padStart(3, 0)}.png`;
+<p class="name">${name}</p>
 
-  const table = openingTable + rows + closingTable;
-  const name = openingP + pokemon["Name"] + closingP;
-  const card =
-    opeingCard +
-    `
-  <div class="card-elements poke-img">
-  <img src="${imgUrl}"/>
-  </div>` +
-    name +
-    table +
-    closingCard;
+<div class="pokemon-detail">
+  <div class="attribute">
+    <div class="label">Types</div>
+    <div class="value">
+    ${typesTemplate}
+    </div>
+  </div>
 
-  return card;
-};
+  <div class="attribute">
+    <div class="label">Weight</div>
+    <div class="value">${weight}</div>
+  </div>
 
-const generateCardRow = (lines) => {
-  const row = lines
-    .map((line) => {
-      return generateCard(line);
-    })
-    .join("");
+  <div class="attribute">
+    <div class="label">HP</div>
+    <div class="value">${hp}</div>
+  </div>
 
-  return `<div class="card-rows">
-              ${row}
-          <\div>`;
+  <div class="attribute">
+    <div class="label">XP</div>
+    <div class="value">${xp}</div>
+  </div>
+
+  <div class="attribute">
+    <div class="label">Attack</div>
+    <div class="value">${attack}</div>
+  </div>
+
+  <div class="attribute">
+    <div class="label">Defense</div>
+    <div class="value">${defense}</div>
+  </div>
+</div>
+</div>`;
 };
 
 const main = () => {
-  const data = fs.readFileSync("./resources.txt", "utf-8");
+  fs.readFile("./resources.txt", "utf-8", (err, data) => {
+    const [header, ...stats] = data.trim().split("\n");
 
-  const [heading, ...lines] = data.trim().split("\n");
+    const cards = stats.map((stat) => generateCard(stat.split("|"))).join("\n");
 
-  let parsedData = "";
-
-  for (index = 0; index < 8; index += 4) {
-    const rows = lines.slice(index, index + 4);
-    parsedData += generateCardRow(rows);
-  }
-
-  console.log(parsedData);
+    console.log(
+      `
+      <div class="card-collections"> 
+      ${cards}
+      </div>
+      `
+    );
+  });
 };
+
 main();
